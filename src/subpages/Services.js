bb2ff43c2214c68c406eb4c688fbe0daa32cc450
Markus Sveggen <marks18@uia.no>
2020-10-19 18:56:20 +0200

    la til yaml fil for hvem er vi seksjon

--- src/subpages/Services.js
+++ src/subpages/Services.js
@@ -3,6 +3,18 @@ import ServiceItem from "../components/ServiceItem"
 import { useStaticQuery, graphql } from "gatsby"
 
 function Services() {
+
+  const { aboutUsYaml } = useStaticQuery(graphql`
+  {
+    aboutUsYaml {
+      maintext
+    }
+  }
+`)
+
+const { maintext } = aboutUsYaml
+
+
   const { allServicesYaml } = useStaticQuery(graphql`
     {
       allServicesYaml {
@@ -15,11 +27,17 @@ function Services() {
       }
     }
   `)
+
   return (
-    <div id = "services" className="container-fluid" style={{ backgroundColor: "#FFCD6C" }}>
+    <div
+      id="services"
+      className="container-fluid"
+      style={{ backgroundColor: "#FFCD6C" }}
+    >
       <div className="container">
         <div className="flexbox">
-          <h1 className="text-center my-5">Services We Offer </h1>
+          <h1 className="text-center my-5">Hvem er vi?</h1>
+          <p className="text-center">{maintext}</p>
           <div className="row">
             {allServicesYaml.edges.map(({ node }) => {
               return <ServiceItem title={node.title} src={node.src} />
