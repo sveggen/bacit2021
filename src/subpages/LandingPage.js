721599837c92188d0814e22daaff84f573b17e04
Markus Sveggen <marks18@uia.no>
2020-10-19 16:30:28 +0200

    la til landing page bilde

--- src/subpages/LandingPage.js
+++ src/subpages/LandingPage.js
@@ -16,7 +16,7 @@ function LandingPage() {
 
   const pageStyle = {
     backgroundImage:
-      "url(https://images.pexels.com/photos/113338/pexels-photo-113338.jpeg?auto=compress&cs=tinysrgb&dpr=2&h=650&w=940)",
+      "url(https://res.cloudinary.com/fishlogger/image/upload/v1603117063/IMG_1335_wrx0vm.jpg)",
     backgroundSize: "cover",
     backgroundRepeat: "no-repeat",
     backgroundPosition: "center",
@@ -33,25 +33,9 @@ function LandingPage() {
     <section id = "home" style={pageStyle} className = "container-fluid">
       <div className="container">
         <Navbar />
-        <div className="centerItems">
-          <h6 style={welcomeStyle}>{welcomeText}</h6>
-          <h1>{landingText}</h1>
-          <button
-            style={{
-              width: "200px",
-              padding: "10px",
-              border: "none",
-              boxShadow: "0px 4px 4px rgba(0, 0, 0, 0.25)",
-              background: "#FFA800",
-              borderRadius: "30px",
-              textTransform: "uppercase",
-              color : "white",
-              fontWeight : "600",
-              marginTop : "20px"
-            }}
-          >
-            Start a Project
-          </button>
+        <div className="lowerLeftItems">
+         <h3 style={welcomeStyle}>{welcomeText}</h3>
+          <h5>{landingText}</h5>
         </div>
       </div>
     </section>
--- src/subpages/Team.js
+++ src/subpages/Team.js
@@ -19,7 +19,7 @@ function Team() {
     <div id = "team" className="container-fluid" style={{ backgroundColor: "#AFECA4" }}>
       <div className="container">
         <div className="flexbox">
-          <h1 className="text-center my-4">Meet Our Team </h1>
+          <h1 className="text-center my-4">Møt vårt team </h1>
           <div className="row my-5">
             {allTeamYaml.edges.map(({ node }) => {
               return <TeamItem title={node.name} src={node.img} />
