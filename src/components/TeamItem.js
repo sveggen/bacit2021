34c3e619dcf3efcc59b2dd9f6114418bbc476e6e
Markus Sveggen <marks18@uia.no>
2020-10-20 21:16:20 +0200

    oppdaterte npm pakker og fikset warnings

--- src/components/TeamItem.js
+++ src/components/TeamItem.js
@@ -6,7 +6,7 @@ function TeamItem({ src, title, text, linkedin }) {
   return (
     <Fade bottom>
       <div className="col-lg-4 my-4 col-sm-12">
-        <img src={src} className="img-fluid rounded-circle" width="200px" />
+        <img src={src} className="img-fluid rounded-circle" width="200px" alt="Profil"/>
         <h3 className="mt-5">{title}</h3>
         <p>{text}</p>
         <SocialIcon className="mr-4" url={"http://linkedin.com/" + linkedin} />
