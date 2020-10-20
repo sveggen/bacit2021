bedb6b24089c25f077f4afb93b44a812d02c4342
Markus Sveggen <marks18@uia.no>
2020-10-21 00:31:18 +0200

    sentrerte diverse elementer

--- src/subpages/Team.js
+++ src/subpages/Team.js
@@ -22,13 +22,13 @@ function Team() {
       <div className="container">
         <div className="flexbox">
           <h1 className="text-center my-4">Møt vårt team </h1>
-          <div className="row my-5">
+          <div className="text-center row my-5">
             {allTeamYaml.edges.map(({ node }) => {
               return <TeamItem title={node.name} src={node.img} text={node.text} linkedin={node.linkedin} />
             })}
+            </div>
           </div>
         </div>
-      </div>
     </div>
   )
 }
