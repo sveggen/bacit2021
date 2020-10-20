1d8f1850a5879beaa2c25d340eb3c25db1e31b4d
Markus Sveggen <marks18@uia.no>
2020-10-20 23:25:00 +0200

    la til toggle på alle meny items

--- src/components/Navbar.js
+++ src/components/Navbar.js
@@ -7,28 +7,29 @@ function Navbar() {
     fontWeight: "600",
     letterSpacing: "0.1em",
   }
+  
   return (
     <div className="pos-f-t">
       <div className="collapse" id="navbarToggleExternalContent">
         <div className="bg-light   p-4">
           <ul class="navbar-nav">
             <li class="nav-item active">
-              <AnchorLink class="nav-link" href="#">
+              <AnchorLink class="nav-link" href="#" data-toggle="collapse" data-target="#navbarToggleExternalContent" aria-controls="navbarToggleExternalContent" aria-expanded="false" aria-label="Toggle navigation">
                 Hjem <span class="sr-only">(current)</span>
               </AnchorLink>
             </li>
-            <li class="nav-item">
-              <AnchorLink class="nav-link" href="#services">
+            <li class="nav-item" >
+              <AnchorLink class="nav-link" href="#services" data-toggle="collapse" data-target="#navbarToggleExternalContent" aria-controls="navbarToggleExternalContent" aria-expanded="false" aria-label="Toggle navigation">
                 Hvem er vi?
               </AnchorLink>
             </li>
             <li class="nav-item">
-              <AnchorLink class="nav-link" href="#team">
+              <AnchorLink class="nav-link" href="#team" data-toggle="collapse" data-target="#navbarToggleExternalContent" aria-controls="navbarToggleExternalContent" aria-expanded="false" aria-label="Toggle navigation">
                 Teamet
               </AnchorLink>
             </li>
             <li class="nav-item">
-              <AnchorLink class="nav-link" href="#end">
+              <AnchorLink class="nav-link" href="#end" data-toggle="collapse" data-target="#navbarToggleExternalContent" aria-controls="navbarToggleExternalContent" aria-expanded="false" aria-label="Toggle navigation">
                 Kontakt Oss
               </AnchorLink>
             </li>
--- src/components/ServiceItem.js
+++ src/components/ServiceItem.js
@@ -2,10 +2,16 @@ import React from "react"
 import Fade from "react-reveal/Fade"
 
 function ServiceItem({ src, title, content }) {
+
+  const style = {
+    justifyContent: 'center',
+    alignItems: 'center',
+  }
+
   return (
     <Fade bottom>
       <div className="col-lg-3 my-4 col-sm-12">
-        <img src={src} width="128px" alt="Ikon"/>
+        <img src={src} width="128px" alt="Ikon" style={style}/>
         <h3 className="mt-5">{title}</h3>
         <p>
          {content}
